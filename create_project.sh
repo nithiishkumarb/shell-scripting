c0ab6d7461dd776c24cc75e8ad9c0b7644e0f3c8
nithiishkumarb <nithiishkumarb@gmail.com>
2026-08-03 01:25:41 +0000

    create folder and files inside that

---
+++ create_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create the folder
+mkdir -p ./create_project
+
+# Goes inside the directory
+cd create_project
+
+# Create 2 files called first and second file
+touch firstfile secondfile
