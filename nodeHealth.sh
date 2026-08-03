fbd2d6ac4efb61f1fffc07f8f29b721c8e08fd3b
nithiishkumarb <nithiishkumarb@gmail.com>
2026-08-03 02:04:34 +0000

    node health init

---
+++ nodeHealth.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+########################################################
+# Author: Nithish
+# Date: 03-08-2026
+#
+#  This script output is for node health monitoring
+#
+#  version: v1
+########################################################
+
+
+set -x # debug mode
+
+# Storage space
+df -h
+
+# RAM space
+free -h
+
+# Number of processors
+nproc
